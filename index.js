cc55d32a6d8e79a5a90ac18fd5bd130dcbc1f4dd
Kunio Okita <chocokunio@gmail.com>
2020-05-22 13:40:50 +0900

    deploy gae settings.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const PORT = 8118
+const PORT = process.env.PORT || 8118;
 
 app.use(express.static('public'));
 
