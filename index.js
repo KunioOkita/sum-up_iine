6744658b4b3c03da38e55098ed70487b984d58fe
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2020-05-22 12:03:57 +0900

    グラフ追加

--- index.js
+++ index.js
@@ -15,20 +15,67 @@ app.use(express.static('public'));
 /**
  * socket.io 処理
  */
+const clientMap = new Map();
+const controllerMap = new Map();
+const countMap = new Map();
+let countTarget = '';
 io.on('connection',function(socket) {
-    console.log('connected');
+    console.log('connected', socket.id);
+    socket.on('disconnect', () => {
+        console.log('disconnect');
+        clientMap.delete(socket.id);
+        controllerMap.delete(socket.id);
+    });
     // click iine button.
     socket.on('iine', (msg) => {
         // count up.
         console.log('iine!');
+        if (countTarget === '') {
+            return;
+        }
+        countMap.set(countTarget, countMap.get(countTarget) + 1);
+    });
+
+    socket.on('client_type', (msg) => {
+        switch(msg) {
+            case 'iine_client':
+            default:
+                clientMap.set(socket.id, socket);
+                break;
+            case 'controller':
+                controllerMap.set(socket.id, socket);
+                break;
+        }
+        console.log(`client_count: ${clientMap.size}`);
+        console.log(`controller_count: ${controllerMap.size}`);
+    });
+
+    // カウント対象を設定する
+    socket.on('set_target', (msg) => {
+        if (!countMap.has(msg)) {
+            countMap.set(msg, 0);
+        }
+        countTarget = msg;
     });
 
     // clear iine.
     socket.on('clear_iine',function(msg){
         // clear iine
-        console.log('message: ' + JSON.stringify(msg));
+        countMap = new Map();
     });
 });
+
+setInterval(() => {
+    let countData = {};
+    for (let [key, value] of countMap) {
+        console.log(key + ' = ' + value);
+        countData[key] = value;
+    }
+
+    for (let [id, socket] of controllerMap) {
+        socket.emit('count_result', countData);
+    }
+}, 1000);
 
 http.listen(PORT, function(){
     console.log('server listening. Port:' + PORT);
--- public/main.js
+++ public/main.js
@@ -1,5 +1,1 @@
-const socket = io();
-socket.on('connect', () => {
-    console.log('connect!!');
-    socket.emit('message', {abc: 'abc'});
-});
+
